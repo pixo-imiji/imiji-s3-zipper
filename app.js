36f4c92489fb6b916223bcbd0dbf46ce60bf3d56
Anas Saber <anastal2001@yahoo.com>
2022-12-17 16:27:07 +0100

    save ack

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ const mongoDbQueue = require("mongodb-queue");
 const getNextMsg = async (queue) => {
     const msg = await (new Promise((resolve, reject) =>  queue.get({visibility: 60 * 60 * 3}, (err, msg) => err ? reject(err) : resolve(msg))));
     if (msg) {
-        console.log("process:", msg.ack);
-        const {payload} = msg;
+        const {payload, ack} = msg;
+        console.log("process:", ack);
         const {partyId, email, isOwner} = payload;
         await zipper(email, partyId, isOwner);
         console.log("sent email to:", email);
-        await (new Promise((resolve, reject) =>  queue.ack(msg.ack, (err, msg) => err ? reject(err) : resolve(msg)))).then();
+        await (new Promise((resolve, reject) =>  queue.ack(ack, (err, msg) => err ? reject(err) : resolve(msg)))).then();
         console.log("deleted job:", msg.ack);
     } else {
         return setTimeout(() => getNextMsg(queue), 2500);
